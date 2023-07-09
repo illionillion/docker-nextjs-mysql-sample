c6c0bff0af4b4976aa8dd453996bf3da338f8335
yuto ichinose <yuto.0526.sano@gmail.com>
2023-07-09 09:56:04 +0900

    サインインのコンポーネントのみ作成

---
+++ src/pages/signin/index.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+const Singin: NextPage = () => {
+    return <>
+        <Head>
+            <title>サインイン</title>
+            <meta name="description" content="サインイン" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+    </>
+}
+
+export default Singin;
